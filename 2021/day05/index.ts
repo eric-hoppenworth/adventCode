ea4e88d9df3145d9da2126f5d74adf4373ff2290
eric hoppenworth <eric-hoppenworth@gmail.com>
2021-12-05 20:36:26 -0500

    day5 complete

--- 2021/day05/index.ts
+++ 2021/day05/index.ts
@@ -16,6 +16,7 @@ type Board = {
     [col: number]: number
   }
 }
+type BoardLineTransformer = (line: Line, board: Board) => Board
 enum LineType {
   HORIZONTAL,
   VERTICAL,
@@ -48,34 +49,29 @@ const getLineType = (line: Line) => {
   return LineType.DIAGONAL
 }
 
-const markBoardLine = (line: Line, board:Board): Board => {
-  const lineType = getLineType(line)
-  if (lineType === LineType.HORIZONTAL) {
-    const startPoint = line.a.col < line.b.col ? line.a : line.b
-    const endPoint = line.a.col < line.b.col ? line.b : line.a
-    for (let i = startPoint.col; i <= endPoint.col; i++) {
-      board = markBoardVector({
-        row: startPoint.row,
-        col: i,
-      }, board)
-    }
-    return board
+const markBoardLinePartOne: BoardLineTransformer = (line: Line, board:Board): Board =>
+  getLineType(line) === LineType.DIAGONAL ? board : markBoardLinePartTwo(line, board)
+
+const markBoardLinePartTwo: BoardLineTransformer = (line: Line, board:Board): Board => {
+  const dRow: number = line.a.row === line.b.row ? 0 :
+    line.a.row > line.b.row ? -1 : 1
+  const dCol: number = line.a.col === line.b.col ? 0 :
+    line.a.col > line.b.col ? -1 : 1
+  let step: number = 0
+  while (
+    (line.a.row + (dRow * step) !== line.b.row) ||
+    (line.a.col + (dCol * step) !== line.b.col)
+  ) {
+    board = markBoardVector({
+      row: line.a.row + (dRow * step),
+      col: line.a.col + (dCol * step),
+    }, board)
+    step++
   }
-  if (lineType === LineType.VERTICAL) {
-    const startPoint = line.a.row < line.b.row ? line.a : line.b
-    const endPoint = line.a.row < line.b.row ? line.b : line.a
-    for (let i = startPoint.row; i <= endPoint.row; i++) {
-      board = markBoardVector({
-        row: i,
-        col: startPoint.col,
-      }, board)
-    }
-    return board
-  }
-  if (lineType === LineType.DIAGONAL) {
-    return board
-  }
-  return board
+  return markBoardVector({
+    row: line.b.row,
+    col: line.b.col,
+  }, board)
 }
 
 const markBoardVector = (vector: Vector, board: Board): Board => {
@@ -89,5 +85,24 @@ const markBoardVector = (vector: Vector, board: Board): Board => {
   return board
 }
 
-const lines = parseInput(testData)
-console.log(markBoardLine(lines[0], {}))
+const scoreBoard = (board: Board): number => {
+  let score = 0
+  for (const row in board) {
+    for (const col in board[row]) {
+      score += board[row][col] > 1 ? 1 : 0
+    }
+  }
+  return score
+}
+
+
+const main = (lines: Line[], markBoardLine: BoardLineTransformer): number => {
+  let board = {}
+  for (let i = 0; i < lines.length; i++) {
+    board = markBoardLine(lines[i], board)
+  }
+  return scoreBoard(board)
+}
+
+console.log(main(parseInput(data), markBoardLinePartOne))
+console.log(main(parseInput(data), markBoardLinePartTwo))
