99dd8517abf00a556498a4f6c7e0f42b52686af3
Eric Hoppenworth <eric.hoppenworth@gmail.com>
2021-12-18 23:49:22 -0500

    day 18

--- 2021/day18/index.ts
+++ 2021/day18/index.ts
@@ -2,141 +2,128 @@ import data from './input.ts'
 import testData from './test.ts'
 
 
-type Target = {
-    x: Range
-    y: Range
-}
-type Range = {
-    min: number
-    max: number
-}
-type Point = {
-    value: number
-    range: Range
+type SnailNumber = string
+
+const parseInput = (input: string): SnailNumber[] => {
+    return input.split('\n')
 }
 
-const parseInput = (input: string): Target => {
-    const matches = input.match(/x=(-?\d*)..(-?\d*), y=(-?\d*)..(-?\d*)/) || []
-    return {
-        x: {
-            min: parseInt(matches[1], 10),
-            max: parseInt(matches[2], 10),
-        },
-        y: {
-            min: parseInt(matches[3], 10),
-            max: parseInt(matches[4], 10),
+const getMagnitude = (snail: SnailNumber): number => {
+    // something something
+    while(snail[0] === '[') {
+        const matches = snail.match(/\[(\d{1,}),(\d{1,})\]/)
+        if (!matches) {
+            return 0
+        }
+        const length = matches[0].length
+        const value = (parseInt(matches[1], 10) * 3) + (parseInt(matches[2], 10) * 2)
+        const index = matches.index || 0
+        snail = `${snail.slice(0, index)}${value}${snail.slice(index + length)}`
+    }
+    return parseInt(snail, 10)
+}
+
+const explode = (snail: SnailNumber, index: number, match: string[]): SnailNumber => {
+    let left = parseInt(match[0] || '0', 10)
+    let right = parseInt(match[1] || '0', 10)
+    const matchLength = match[0].length + match[1].length + 1
+    // look backward (?) until I find a number
+    // because of the way the rules work out, I can be sure the number is a single digit~~~~~OOPS
+    let previousIndex = 0
+    let previousLength = 0
+    for (let i = index - 1; i >= 0; i--) {
+        const isDigit = Boolean(snail[i].match(/\d/))
+        if (isDigit) {
+            previousIndex = i
+            previousLength++
+        }
+        if (!isDigit && previousLength) {
+            break;
         }
     }
+    if (previousLength) {
+        left += parseInt(snail.slice(previousIndex, previousIndex + previousLength), 10)
+    }
+    let nextIndex = 0
+    let nextLength = 0
+    // looking forward, it _could_ be a two digit number, so we'll use regex
+    let nextMatch = snail.slice(index + matchLength).match(/\d{1,}/)
+    if (nextMatch) {
+        right += parseInt(nextMatch[0], 10)
+        nextLength = nextMatch[0].length
+        nextIndex = (nextMatch.index || 0) + index + matchLength
+    }
+
+    const front = previousLength ? `${snail.slice(0, previousIndex)}${left}${snail.slice(previousIndex + previousLength, index - 1)}` : snail.slice(0, index - 1)
+    const end = nextLength ?
+        `${snail.slice(index + matchLength + 1, nextIndex)}${right}${snail.slice(nextIndex + nextLength)}` :
+        `${snail.slice(index + matchLength + 1)}`
+    return `${front}0${end}`
+}
+const split = (snail: SnailNumber, index: number, match: string): SnailNumber => {
+    const length = match.length
+    const value = parseInt(match, 10)
+    return `${snail.slice(0,index)}[${Math.floor(value/2)},${Math.ceil(value/2)}]${snail.slice(index + length)}`
 }
 
-const partOne = (target: Target): number => {
-    // constant gravity means that what comes up, must come down to the same altitude
-    // this means that the best height is achieved when we just barely hit the lowest point of the target
-    const minimum = Math.abs(target.y.min)
-    return (minimum * (minimum - 1)) / 2
-}
-//
-// pos = 0
-// velocity = 0
-// a = -1
-//
-// step 1 => 0, -1,
-// step 2 => -1, -2,
-// setp 3 => -3, -3,
-// step 4 => -6, -4
-// step 5 => -10, -5
-const getRowStepCount = (row: number, range: Range): Range => {
-    const a = -1
-    let v = row
-    let position = 0
-    let minSteps = 0
-    let steps = 0
-    let maxSteps = 0
+const sumSnailNumbers = (left: SnailNumber, right: SnailNumber): SnailNumber => {
+    let total: string = `[${left},${right}]`
+    let action = true
+    while(action) {
+        action = false
+        let depth = 0
+        for (let i = 0; i < total.length; i++) {
+            const character = total[i]
+            if (character === '[') {
+                depth++
+                continue
+            } else if (character === ']') {
+                depth--
+                continue
+            }
 
-    while (position >= range.min) {
-        maxSteps = steps
-        position = position + v
-        v = v + a
-
-        steps++
-        if (position <= range.max && !minSteps) {
-            minSteps = steps
+            if (depth > 4) {
+                const matches = total.slice(i).match(/^\d{1,},\d{1,}/)
+                if (matches) {
+                    total = explode(total, i, matches[0].split(','))
+                    action = true
+                    break
+                }
+            }
         }
-    }
-    return {
-        min: minSteps,
-        max: maxSteps,
-    }
-}
-const getColStepCount = (col: number, range: Range): Range => {
-    let velocity = col
-    let position = 0
-    let steps = 0
-    let minSteps = 0
-    let maxSteps = 0
-    while (position <= range.max) {
-        maxSteps = steps
-        position = position + velocity
-        velocity = velocity - 1
-        steps++
-        if (position >= range.min && !minSteps) {
-            minSteps = steps
-        }
-        if (velocity <= 0) {
-            maxSteps = Infinity
-            break
-        }
-    }
-    return {
-        min: minSteps,
-        max: maxSteps,
-    }
-}
-
-const partTwo = (target: Target): number => {
-    const rows: Point[] = []
-    for(let i = target.y.min; i < Math.abs(target.y.min); i++) {
-        const range = getRowStepCount(i, target.y)
-        if (range.min > range.max || range.min === 0) {
+        if (action) {
             continue
         }
-        rows.push({
-            value: i,
-            range,
-        })
-    }
-    const cols: Point[] = []
-    for (let i = 1; i <= target.x.max; i ++) {
-        if (i*(i+1)/2 < target.x.min) {
-            continue
-        }
-        const range = getColStepCount(i, target.x)
-        if (range.min > range.max || range.min === 0) {
-            continue
-        }
-        cols.push({
-            value: i,
-            range,
-        })
-    }
-    let count = 0
-    rows.forEach(row => console.log(row))
-    console.log(cols)
-    // since columns can have Infinity, I want to loop over my rows instead
-    for (const yPoint of rows) {
-        // find the number of xPoints that have the "same" step count
-        // yMin is higher than or equal to xMin AND yMin is less than or equal to xMax
-        for (const xPoint of cols) {
-            if (yPoint.range.min >= xPoint.range.min && yPoint.range.min <= xPoint.range.max) {
-                count++
-            } else if (yPoint.range.max >= xPoint.range.min && yPoint.range.max <= xPoint.range.max) {
-                count++
+        for (let i = 0; i < total.length; i++) {
+            const matches = total.slice(i).match(/^\d{1,}/)
+            if (matches && parseInt(matches[0], 10) >= 10) {
+                total = split(total, i, matches[0])
+                action = true
+                break
             }
         }
     }
-    return count
+    return total
+}
+
+const partOne = (list: SnailNumber[]): number => {
+    let sum = list[0]
+    for (let i = 1; i < list.length; i++) {
+        sum = sumSnailNumbers(sum, list[i])
+    }
+    return getMagnitude(sum)
+}
+const partTwo = (list: SnailNumber[]): number => {
+    const magnitudes = []
+    for (let i = 0; i < list.length; i++) {
+        for (let j = 0; j < list.length; j++) {
+            if (i === j) {
+                continue
+            }
+            magnitudes.push(getMagnitude(sumSnailNumbers(list[i], list[j])))
+        }
+    }
+    return Math.max(...magnitudes)
 }
 // console.log(partOne(parseInput(data)))
 console.log(partTwo(parseInput(data)))
-
-// console.log(getColStepCount(19, { min: 20, max: 30 }))
