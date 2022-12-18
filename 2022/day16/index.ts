eb5e42ab561bd8657faf4fb40f30f9b4e3c215be
eric hoppenworth <eric-hoppenworth@gmail.com>
2022-12-18 20:40:34 +0000

    day 16

--- 2022/day16/index.ts
+++ 2022/day16/index.ts
@@ -11,12 +11,12 @@ type DistanceRecord = Record<string, number>;
 type Cave = Record<string, Cavern>
 type Unit = {
   name: string;
-  location: Cavern;
+  location: string;
   remainingSteps: number;
 }
 type Path = {
   units: Unit[];
-  remainingNodes: Cavern[];
+  remainingNodes: string[];
   score: number;
 }
 
@@ -59,8 +59,9 @@ const getDistances = (cave: Cave, start: string) => {
   return moveBoard
 }
 
-const getPaths = (path: Path): Path[] => {
-  return path.remainingNodes.map((cavern): false | Path => {
+const getPaths = (cave: Cave, path: Path): Path[] => {
+  return path.remainingNodes.map((cavernName: string): false | Path => {
+    const cavern: Cavern = cave[cavernName]
     // I should move the unit with the large number of steps remaining
     let unitToMove: Unit = { ...path.units[0] }
     let maxRemaining = -Infinity
@@ -73,22 +74,22 @@ const getPaths = (path: Path): Path[] => {
     if (!unitToMove) {
       return false
     }
-    const stepsToOpen = unitToMove.location.distances[cavern.name] + 1
+    const stepsToOpen = cave[unitToMove.location].distances[cavern.name] + 1
     const remainingSteps = unitToMove.remainingSteps - stepsToOpen
     if (remainingSteps < 0) {
       return false
     }
     unitToMove.remainingSteps = remainingSteps
-    unitToMove.location = cavern
+    unitToMove.location = cavern.name
     return {
       units: path.units.map((unit) => unit.name === unitToMove.name ? unitToMove : unit),
-      remainingNodes: path.remainingNodes.filter(a => a !== cavern),
+      remainingNodes: path.remainingNodes.filter(a => a !== cavernName).sort(),
       score: path.score + (cavern.rate * remainingSteps),
     }
   }).filter(Boolean) as Path[]
 }
 
-const partOne = (input: string): number => {
+const partOne = (input: string, units: string[]): number => {
   const cave = parseInput(input)
   const nodes: Cavern[] = Object.values(cave).filter((cavern) => cavern.rate)
   cave['AA'].distances = getDistances(cave, 'AA')
@@ -97,81 +98,25 @@ const partOne = (input: string): number => {
   })
   // now, starting from AA, I want to move to each node independantly
   const startPath = {
-    units: [{ name: 'ME', location: cave['AA'], remainingSteps: 30 }],
-    remainingNodes: nodes,
+    units: units.map((name) => ({ name, location: 'AA', remainingSteps: 30 - (4 * (units.length - 1)) })),
+    remainingNodes: nodes.map(a => a.name).sort(),
     score: 0,
   }
   let queue: Path[] = [startPath]
-  const finalPaths: Path[] = []
+  let maxScore: number = -Infinity
   while(queue.length) {
     const path = queue.pop() as Path
-    const newPaths = getPaths(path)
+    const newPaths = getPaths(cave, path)
     if (!newPaths.length) {
-      finalPaths.push(path)
+      if (path.score > maxScore) {
+        maxScore = path.score
+      }
     } else {
       queue = queue.concat(newPaths)
     }
   }
-  return Math.max(...finalPaths.map(a => a.score))
+  return maxScore
 }
 
-const partTwo = async (input: string) => {
-  const cave = parseInput(input)
-  const nodes: Cavern[] = Object.values(cave).filter((cavern) => cavern.rate)
-  cave['AA'].distances = getDistances(cave, 'AA')
-  nodes.forEach(cavern => {
-    cavern.distances = getDistances(cave, cavern.name)
-  })
-  // now, starting from AA, I want to move to each node independantly
-  const startPath = {
-    units: [{ name: 'ME', location: cave['AA'], remainingSteps: 26 }, { name: 'ELEPHANT', location: cave['AA'], remainingSteps: 26 }],
-    remainingNodes: nodes,
-    score: 0,
-  }
-  let queue: Path[] = [startPath]
-  const finalPaths: Path[] = []
-  // let paths = getPaths(startPath)
-  // for (let i = 0; i < nodes.length; i++) {
-  //   console.log(paths)
-  //   paths = getPaths(paths[paths.length - 1])
-  // }
-  // console.log(paths)
-  let count = 0
-  while(queue.length && count < 10000) {
-    const path = queue.pop() as Path
-    const newPaths = getPaths(path)
-    if (!newPaths.length) {
-      finalPaths.push(path)
-    } else {
-      // check each new path to see if it needs to be added to the queue
-      // for (const newPath of newPaths) {
-      //   for (const queuedPath of queue) {
-      //     console.log('checking...')
-      //     const newRemaining = newPath.remainingNodes.map(a => a.name).sort().join()
-      //     const queuedRemaining = queuedPath.remainingNodes.map(a => a.name).sort().join()
-      //     console.log(newRemaining)
-      //     console.log(queuedRemaining)
-      //     if (newRemaining === queuedRemaining) {
-      //       console.log('matched!')
-      //     }
-      //     // paths are the same if
-      //       // remainingNodes is equal
-      //       // score is equal (?)
-      //
-      //   }
-      // }
-      queue = queue.concat(newPaths)
-    }
-    count++
-    if (count % 500000 === 0) {
-      console.log(queue.length)
-      console.log(finalPaths.length)
-    }
-  }
-  await Deno.writeTextFile("./real-queue.txt", JSON.stringify(queue, null, 2));
-  return Math.max(...finalPaths.map(a => a.score))
-}
-
-
-console.log(partOne(data))
-// console.log(partTwo(data))
+// console.log(partOne(testData, ['ME']))
+console.log(partOne(data, ['ME', 'ELEPHANT']))
